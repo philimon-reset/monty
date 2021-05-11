7828df531ac582b87210ac876826c72ff6533f1a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-11 05:16:44 +0300

    Update 0-print_dlistint.c

--- 0-print_dlistint.c
+++ 0-print_dlistint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "monty.h"
 
 /**
  * print_dlistint - print all the elements in the list.
@@ -7,9 +7,9 @@
  * Return: returns the number of nodes.
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const stack_t *h)
 {
-	const dlistint_t *temp = h;
+	const stack_t *temp = h;
 	int nodes = 0;
 
 	while (temp != NULL)
