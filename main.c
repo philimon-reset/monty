e996d5be263d60859be02ed68f7a0ca32d2f0bde
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-11 14:38:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
 #include "monty.h"
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]))
+{
+	int line_n = 0;
+	char *line = NULL;
+	FILE *stream;
+	size_t num = 0;
+
+	if (argc != 2)
+	{
+		fprint(strerr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	stream = fopen(argv[1], 'r');
+	if (stream == NULL)
+	{
+		/** Error: Can't open file <file> **/
+		exit(EXIT_FAILURE);
+	}
+	if (getline(&line, &num, stream) != -1)
+	{
+		;
+	}
+	return (0);
+}
