1d3b7bcfca60d6fcc6594a4e3c37fe4aaf77fb4b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-12 05:55:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -10,10 +10,12 @@
 
 int main(int argc, char *argv[]))
 {
-	int line_n = 0;
+	int line_n = 1;
 	char *line = NULL;
+	char *value = NULL, *p, *temp;
 	FILE *stream;
 	size_t num = 0;
+	size_t line_len;
 
 	if (argc != 2)
 	{
@@ -26,9 +28,51 @@ int main(int argc, char *argv[]))
 		/** Error: Can't open file <file> **/
 		exit(EXIT_FAILURE);
 	}
-	if (getline(&line, &num, stream) != -1)
+	while ((line_len = getline(&line, &num, stream)) != -1)
 	{
-		;
+		if (p = valid(line))
+		{
+			strcpy(temp, p);
+			value = p + strlen(p) - 1;
+			if (*value == '\n')
+			{
+				*value = 0;
+			}
+			/** value plus other string, check if first character is space and second character is int **/
+			if (*temp != ' ')
+			{
+				;
+			}
+			temp += strspn(temp, ' ');
+		}
+		else
+		{
+			line -= strspn(line, ' ');/** find a way to get the first string of the opcode **/
+			fprint(strerr, "L%d: unknown instruction %s\n", line_n, line);
+			exit(EXIT_FAILURE);
+		}
+		line_n++;
 	}
 	return (0);
+}
+
+/**
+ * checker - checks if value given is valid
+ * @line: current line to be checked
+ *
+ * Return: stack to be added if valid
+ */
+char **checker(char *line)
+{
+	
+
+	if (p = valid(line))
+	{
+		value = p + strlen(p) - 1;
+		if (*value == '\n')
+		{
+			*value = 0;
+		}
+		return (p); /** value plus other string, check if first character is space and second character is int **/
+	}
 }
