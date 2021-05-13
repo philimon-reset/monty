4a17ae3754af9158d2dcdd052110a027020bdd19
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-13 21:14:31 +0300

    Update helper3.c

--- helper3.c
+++ helper3.c
@@ -10,8 +10,13 @@
 
 void pint(stack_t **stack, unsigned int ln __attribute__ ((unused)))
 {
-	const stack_t *temp = *stack;
+	stack_t *temp = *stack;
 
+	if (stack_len(*stack) == 0)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_n);
+		exit(EXIT_FAILURE);
+	}
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
