6ed106388d8ebc583bba4045538b725a3dfd6ba3
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-13 22:19:46 +0300

    Update helper3.c

--- helper3.c
+++ helper3.c
@@ -29,7 +29,7 @@ void pint(stack_t **stack, unsigned int ln)
  * @stack: head of list.
  * @ln: index to delete.
  *
- * Return: 1 if successs -1 if not
+ * Return: void function
  */
 void pop(stack_t **stack, unsigned int ln)
 {
@@ -43,5 +43,33 @@ void pop(stack_t **stack, unsigned int ln)
 	*stack = temp->next;
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
+	free(temp);
+}
+
+/**
+ * swap - swaps the top two elements of the stack.
+ * @stack: head of list.
+ * @ln: index to delete.
+ *
+ * Return: void function
+ */
+void swap(stack_t **stack, unsigned int ln)
+{
+	stack_t *temp = *stack;
+	int i = 0;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", ln);
+		exit(EXIT_FAILURE);
+	}
+	while (temp != NULL)
+	{
+		i++;
+		temp = temp->next;
+		stack = stack->next;
+	}
+	((*stack)->prev)->n = temp->n;
+	(*stack)->n = (temp->prev)->n;
 	free(temp);
 }
