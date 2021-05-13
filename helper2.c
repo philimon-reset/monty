91ed49d96ff844d22cef6e63da1325d5cf7c084d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-13 21:13:20 +0300

    Update helper2.c

--- helper2.c
+++ helper2.c
@@ -124,14 +124,6 @@ void error_p(char *line, int line_n, stack_t **stack)
 		fprintf(stderr, "L%d: usage: push integer\n", line_n);
 		exit(EXIT_FAILURE);
 	}
-	if ((strncmp(line, "pint", 4) == 0) && stack_len(*stack) == 0)
-	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_n);
-		free_stack(*stack);
-		free(val);
-		free(line);
-		exit(EXIT_FAILURE);
-	}
 	while (a < 2)
 	{
 		if (strncmp(line, av[a], 4) == 0)
