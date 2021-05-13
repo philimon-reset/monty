bf1cb3ded92c35a1f3a0ec85590cc7f385f82fc5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-13 20:42:19 +0300

    Update helper3.c

--- helper3.c
+++ helper3.c
@@ -1,1 +1,21 @@
-#
+#include "monty.h"
+
+/**
+ * push - add to the begining of the list.
+ * @stack: head of list.
+ * @line_number: line_number.
+ *
+ * Return: void function.
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	const stack_t *temp = *stack;
+	int nodes = 0;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		break;
+	}
+}
