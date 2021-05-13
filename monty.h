ab60a6464c50e2947b8c8185fc0abbe7ab5637bc
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-13 20:28:34 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -38,6 +38,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void argcc(int argc_n);
 void checker(char *line);
 int token(char *s);
 /** helper **/
