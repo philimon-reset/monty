c28994cd441dfc9831903b40590265129c44dce0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-13 17:52:38 +0300

    Update 00.m

--- 00.m
+++ 00.m
@@ -8,8 +8,8 @@ push 2
 
                            
 push 4
-$
+
     push 5    
       push    6        
 
-pall This is the end of our program. Monty is awesome!$
+pall This is the end of our program. Monty is awesome!
