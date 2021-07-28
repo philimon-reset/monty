ffd998cb9090cdb9480a76da7937f15831cd22d1
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-28 19:11:44 +0300

    Update utili3.c

--- utili3.c
+++ utili3.c
@@ -32,6 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			for (i = 0; i < min(old_size, new_size); i++)
 				*((char *)temp_block + i) = *((char *)ptr + i);
+			free(ptr);
 			return (temp_block);
 		}
 		else
@@ -46,50 +47,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
  *
  * Return: return the minimum
  */
-unsigned int min(unsigned int a, unsigned int b)
+unsigned min(unsigned int a, unsigned int b)
 {
 	if (a < b)
 		return (a);
 	return (b);
-}
-
-/**
- * checker - checks if value given is valid
- * @line: current line to be checked
- *
- * Return: stack to be added if valid
- */
-void checker(char *line)
-{
-	int i = 0;
-
-	while (line[i] != '\0')
-	{
-		if (line[i] < ' ' || line[i] > 126)
-			break;
-		i++;
-	}
-	line[i] = '\0';
-}
-/**
- * token - checks if value given is valid
- * @s: current line to be checked
- *
- * Return: 1 or 0
- */
-
-int token(char *s)
-{
-	char *temp;
-	char *token;
-
-	temp = strdup(s);
-	token = strtok(temp, " ");
-	if (token == NULL)
-	{
-		free(temp);
-		return (0);
-	}
-	free(temp);
-	return (1);
 }
